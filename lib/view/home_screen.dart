9a672131a0e71ce3fdd7c92852e70a14bf3acae1
Sikandar897 <sikandarafridi6@gmail.com>
2023-12-20 23:46:42 +0400

    headlines of  BBC, UI, APi integration

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:my_news_app/models/news_channel_headlines_model.dart';
+import 'package:my_news_app/utils/const.dart';
+import 'package:my_news_app/view_model/news_view_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +15,157 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final format = DateFormat('MMM dd, yyyy');
+  NewsViewMOdel newsViewMOdel = NewsViewMOdel();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //inititlazing our media queries
+    final width = MediaQuery.sizeOf(context).width * 1;
+    final height = MediaQuery.sizeOf(context).height * 1;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/category_icon.png',
+            width: 30,
+            height: 30,
+          ),
+        ),
+        centerTitle: true,
+        title: Text(
+          'News',
+          style: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: ListView(
+        //horizantal list goes here
+        children: [
+          SizedBox(
+              height: height * .55,
+              width: width,
+              child: FutureBuilder<NewsChannelsHeadlinesModel>(
+                  future: newsViewMOdel.fetchNewsChannelHeadlinesAPi(),
+                  builder: (BuildContext context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: SpinKitCircle(
+                          size: 50,
+                          color: Colors.blue,
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data!.articles!.length,
+                          itemBuilder: (context, index) {
+                            DateTime dateTime = DateTime.parse(snapshot
+                                .data!.articles![index].publishedAt
+                                .toString());
+                            return SizedBox(
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    height: height * 0.6,
+                                    width: width * 0.9,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: width * .04,
+                                        vertical: height * 0.02),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: CachedNetworkImage(
+                                          imageUrl: snapshot
+                                              .data!.articles![index].urlToImage
+                                              .toString(),
+                                          fit: BoxFit.cover,
+                                          placeholder: (context, url) =>
+                                              Container(child: spinkit2),
+                                          errorWidget: (context, url, error) =>
+                                              const Icon(
+                                                Icons.error_outline,
+                                                color: Colors.red,
+                                              )),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 20,
+                                    child: Card(
+                                      elevation: 5,
+                                      color: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: Container(
+                                        alignment: Alignment.bottomCenter,
+                                        padding: const EdgeInsets.all(15),
+                                        height: height * .22,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: [
+                                            SizedBox(
+                                              width: width * 0.7,
+                                              child: Text(
+                                                snapshot.data!.articles![index]
+                                                    .title
+                                                    .toString(),
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 17,
+                                                    fontWeight:
+                                                        FontWeight.w700),
+                                              ),
+                                            ),
+                                            const Spacer(),
+                                            SizedBox(
+                                              width: width *0.7,
+                                              child: Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                children: [
+                                                  Text(
+                                                    snapshot.data!
+                                                        .articles![index].source!.name
+                                                        .toString(),
+                                                    maxLines: 2,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 13,
+                                                        fontWeight:
+                                                            FontWeight.w600),
+                                                  ),
+                                                  Text( format.format(dateTime)
+                                                        ,
+                                                    maxLines: 2,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    style: GoogleFonts.poppins(
+                                                        fontSize: 12,
+                                                        fontWeight:
+                                                            FontWeight.w500),
+                                                  ),
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          });
+                    }
+                  }))
+        ],
+      ),
+    );
   }
 }

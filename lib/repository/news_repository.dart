beab1157eff293aff41f63b50d4992c6a8f17dfb
Sikandar897 <sikandarafridi6@gmail.com>
2023-12-22 01:44:16 +0400

    popup menu and its implemetion

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -2,25 +2,21 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:my_news_app/models/news_channel_headlines_model.dart';
-//this class will fetch our api's data 
 
-class NewsRepository{
+class NewsRepository {
+  Future<NewsChannelsHeadlinesModel> fetchNewsChannelHeadlinesAPi(String name) async {
+    
+    // Use the provided 'name' parameter to dynamically set the news source in the API URL
+    String url = 'https://newsapi.org/v2/top-headlines?sources=$name&apiKey=efb4c68f13614ce5be1054a64a4d336b';
 
-Future<NewsChannelsHeadlinesModel> fetchNewsChannelHeadlinesAPi() async{
+    final response = await http.get(Uri.parse(url));
 
-  //make sure to store this token in a safe storage like sharedprefrence if it leaks any one can miss used it. just a hint for future dev
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body.toString());
+      return NewsChannelsHeadlinesModel.fromJson(body);
+    }
 
-  String url = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=efb4c68f13614ce5be1054a64a4d336b' ;
-
-  final response = await http.get(Uri.parse(url));
-
-  if(response.statusCode == 200){
-    final body = jsonDecode(response.body.toString());
-    return NewsChannelsHeadlinesModel.fromJson(body);
+    // If the API call is not successful, throw an exception
+    throw Exception('Error');
   }
-  throw Exception('Error');
-
-
-}
-
 }
--- lib/view_model/news_view_model.dart
+++ lib/view_model/news_view_model.dart
@@ -1,11 +1,11 @@
 import 'package:my_news_app/models/news_channel_headlines_model.dart';
 import 'package:my_news_app/repository/news_repository.dart';
 
-class NewsViewMOdel{
+class NewsViewMOdel {
   final _repo = NewsRepository();
 
-  Future<NewsChannelsHeadlinesModel> fetchNewsChannelHeadlinesAPi() async{
-    final response = await _repo.fetchNewsChannelHeadlinesAPi();
+  Future<NewsChannelsHeadlinesModel> fetchNewsChannelHeadlinesAPi(String name) async {
+    final response = await _repo.fetchNewsChannelHeadlinesAPi(name);
     return response;
   } 
 }
